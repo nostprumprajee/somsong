aa11df30214a86af9c689386160b20456ec9e096
kesano <gun_ini3@hotmail.com>
2016-11-09 16:44:31 +0700

    dfdsf
    sdfsdf

--- app/src/main/java/com/example/lenovo/somsong/Tryss.java
+++ app/src/main/java/com/example/lenovo/somsong/Tryss.java
@@ -45,6 +45,7 @@ public class Tryss extends Activity {
         ImageButton ssdebut = (ImageButton)findViewById(R.id.button13);
         Intent intent3 = new Intent(Tryss.this,Camw3.class);
         startActivity(intent3);
+        int a=0;
     }
 
 
