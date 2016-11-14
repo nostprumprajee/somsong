80d4e2ed37c05304a4e613fb149974b43c3eda87
kesano <gun_ini3@hotmail.com>
2016-11-15 00:47:55 +0700

    พังๆ อะไรไม่รู้แม่ม -0-

--- app/src/main/java/com/example/lenovo/somsong/Camm1.java
+++ app/src/main/java/com/example/lenovo/somsong/Camm1.java
@@ -63,43 +63,9 @@ public class Camm1 extends Activity implements SurfaceHolder.Callback {
         mPreview = (SurfaceView)findViewById(R.id.preview);
         mPreview.getHolder().addCallback(this);
         mPreview.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+//   *** end old ****
 
-        //facedetect****
-        SurfaceView myImageView = (SurfaceView) findViewById(R.id.preview);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable=true;
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.id.preview,
-                options);
-        Paint myRectPaint = new Paint();
-        myRectPaint.setStrokeWidth(5);
-        myRectPaint.setColor(Color.RED);
-        myRectPaint.setStyle(Paint.Style.STROKE);
-        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
-        Canvas tempCanvas = new Canvas(tempBitmap);
-        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
 
-        FaceDetector faceDetector = new
-                FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
-                .build();
-        if(!faceDetector.isOperational()){
-
-            return;
-        }
-
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Face> faces = faceDetector.detect(frame);
-
-        for(int i=0; i<faces.size(); i++) {
-            Face thisFace = faces.valueAt(i);
-            float x1 = thisFace.getPosition().x;
-            float y1 = thisFace.getPosition().y;
-            float x2 = x1 + thisFace.getWidth();
-            float y2 = y1 + thisFace.getHeight();
-            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-        }
-        // end facedetection
 
 
     }
